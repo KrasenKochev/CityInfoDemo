5a146b08e3c92ca07e5918b85393766002a44233
Krasen Kochev <Krasen.Kochev@kallidus.com>
2025-07-18 16:32:03 +0300

    seeding the database , adding file for DB console commands

--- CityInfo.API/DbContexts/CityInfoContext.cs
+++ CityInfo.API/DbContexts/CityInfoContext.cs
@@ -13,6 +13,43 @@ namespace CityInfo.API.DbContexts
 		{
 		}
 
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+
+
+			modelBuilder.Entity<City>()
+				.HasData(
+				new City("Lovech dage")
+				{
+					Id = 1,
+					Description = "The hometown"
+				},
+				new City("Pleven da-ge")
+				{
+					Id = 2,
+					Description = "The da-ge original city"
+				}
+				);
+
+			modelBuilder.Entity<PointOfInterest>()
+				.HasData(
+				new PointOfInterest("Home")
+				{
+					Id = 1,
+					CityId =1,
+					Description = "The home"
+				},
+				new PointOfInterest("zooPark")
+				{
+					Id = 2,
+					CityId =2,
+					Description = "The animal zoo"
+				}
+				);
+
+
+			base.OnModelCreating(modelBuilder);
+		}
 
 		//protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		//{
---
+++ CityInfo.API/Migrations/20250718132645_InitialDataSeed.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CityInfo.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDataSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "The hometown", "Lovech dage" },
+                    { 2, "The da-ge original city", "Pleven da-ge" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "PointsOfInterest",
+                columns: new[] { "Id", "CityId", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "The home", "Home" },
+                    { 2, 2, "The animal zoo", "zooPark" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
